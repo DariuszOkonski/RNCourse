6b0286b322e5bff4392f54374d021d9132b5df14
Dariusz Okonski <darek200180@gmail.com>
2026-08-31 05:55:33 +0200

    display users goals

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   }
 
   function addGoalHandler() {
-    console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
   }
 
   return (
@@ -24,7 +28,9 @@ export default function App() {
       </View>
 
       <View style={styles.goalsContainer}>
-        <Text>List of goal...</Text>
+        {courseGoals.map((goal, index) => (
+          <Text key={index}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
